9ea5c0f0d4cace3051f129f398a97b1e879e72f8
acalcutt <acalcutt@techidiots.net>
2018-12-23 23:38:49 -0500

    code to create osm sql user for import actions

--- import_pdb.sh
+++ import_pdb.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
-POSTGRES_DB=osm
-POSTGRES_USER=postgres
-POSTGRES_HOST=localhost
-POSTGRES_PORT=5432
-set PGPASSWORD=password
+. config/env.config
 
-psql -h localhost --username="$POSTGRES_USER" <<EOSQL
+set PGPASSWORD=$POSTGRES_PASS
+psql -h $POSTGRES_HOST --username="$POSTGRES_USER" <<EOSQL
 	DROP DATABASE IF EXISTS $POSTGRES_DB;
-    create database $POSTGRES_DB;
+    CREATE DATABASE $POSTGRES_DB;
 	\c $POSTGRES_DB;
+	DROP USER IF EXISTS $OSM_USER;
+	CREATE ROLE $OSM_USER LOGIN PASSWORD '$OSM_PASS' NOSUPERUSER NOCREATEROLE CREATEDB;
     CREATE EXTENSION postgis;
     CREATE EXTENSION hstore;
 EOSQL
@@ -17,6 +16,6 @@ if [ ! -f data/planet-latest.osm.pbf ]; then
     wget http://planet.osm.org/pbf/planet-latest.osm.pbf -P data
 fi
 
-bin/imposm import -config config/config.json -mapping config/mapping.yml -write
+bin/imposm import -mapping config/mapping.yml -write -connection postgis://$POSTGRES_USER:$POSTGRES_PASS@$POSTGRES_HOST/$POSTGRES_DB
 bin/imposm import -config config/config.json -read data/planet-latest.osm.pbf -write -optimize -overwritecache -diff
 bin/imposm import -config config/config.json -deployproduction
