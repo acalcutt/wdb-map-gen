5b37664fe8235cad4dbf84c0c22032a198579dd0
acalcutt <acalcutt@worcester.edu>
2018-12-30 02:15:15 -0500

    updated load and export

--- export_mbtiles.sh
+++ export_mbtiles.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 . config/env.config
 
+CONFIG_DIR=$(pwd)/config
 EXPORT_DIR=$(pwd)/data
+LAYERS_CONFIG=$CONFIG_DIR/wdbmap.yaml
 DEST_PROJECT_DIR=$EXPORT_DIR/openmaptiles.tm2source
 mkdir -p $EXPORT_DIR
 mkdir -p $DEST_PROJECT_DIR
 
 #Create TM2 SOURCE DIR
-openmaptiles-tools/bin/generate-tm2source config/openmaptiles.yaml --host="$POSTGRES_HOST" --port=$POSTGRES_PORT --database="$POSTGRES_DB" --user="$OSM_USER" --password="$OSM_PASS" > $DEST_PROJECT_DIR/data.yml
-openmaptiles-tools/bin/generate-imposm3 config/openmaptiles.yaml > $EXPORT_DIR/mapping.yaml
-openmaptiles-tools/bin/generate-sql config/openmaptiles.yaml > $EXPORT_DIR/tileset.sql
+openmaptiles-tools/bin/generate-tm2source $LAYERS_CONFIG --host="$POSTGRES_HOST" --port=$POSTGRES_PORT --database="$POSTGRES_DB" --user="$POSTGRES_USER" --password="$POSTGRES_PASS" > $DEST_PROJECT_DIR/data.yml
+openmaptiles-tools/bin/generate-sql $LAYERS_CONFIG > $EXPORT_DIR/tileset.sql
 
 PGPASSWORD=$POSTGRES_PASS psql -h $POSTGRES_HOST -d $POSTGRES_DB -U "$POSTGRES_USER" -f postgis-vt-util/postgis-vt-util.sql
-PGPASSWORD=$POSTGRES_PASS psql -h $POSTGRES_HOST -d $POSTGRES_DB -U "$POSTGRES_USER" -f $EXPORT_DIR/language.sql
+PGPASSWORD=$POSTGRES_PASS psql -h $POSTGRES_HOST -d $POSTGRES_DB -U "$POSTGRES_USER" -f $CONFIG_DIR/language.sql
 PGPASSWORD=$POSTGRES_PASS psql -h $POSTGRES_HOST -d $POSTGRES_DB -U "$POSTGRES_USER" -f $EXPORT_DIR/tileset.sql
 
 #Export to MBTILES tileset
--- load_planet.sh
+++ load_planet.sh
@@ -1,32 +1,52 @@
 #!/bin/bash
 . config/env.config
 
-echo "====> : Start importing Planet OpenStreetMap data: ./data/planet-latest.osm.pbf -> imposm3[./config/mapping.yaml] -> PostgreSQL"
+readonly DIFF_MODE=${DIFF_MODE:-true}
+PBF=planet-latest.osm.pbf
+PBF_BASE=http://planet.osm.org/pbf/
+#PBF=massachusetts-latest.osm.pbf
+#PBF_BASE=https://download.geofabrik.de/north-america/us/
+
+echo "====> : Start importing Planet OpenStreetMap data: data/$PBF -> imposm3[./config/mapping.yaml] -> PostgreSQL"
 
 CONFIG_DIR=$(pwd)/config
 EXPORT_DIR=$(pwd)/data
-IMPSOSM3_CACHE_DIR=$EXPORT_DIR/imposm3_cache
+LAYERS_CONFIG=$CONFIG_DIR/wdbmap.yaml
+IMPOSM3_MAPPING=$CONFIG_DIR/mapping.yaml
+IMPOSM3_CONFIG=$CONFIG_DIR/imposm.json
+IMPOSM3_CACHE_DIR=$EXPORT_DIR/imposm3_cache
+IMPOSM3_DIFF_DIR=$EXPORT_DIR/imposm3_diff
 mkdir -p $EXPORT_DIR
-mkdir -p $IMPSOSM3_CACHE_DIR
+mkdir -p $IMPOSM3_CACHE_DIR
+mkdir -p $IMPOSM3_DIFF_DIR
 
+#Create the mapping file
+openmaptiles-tools/bin/generate-imposm3 $LAYERS_CONFIG > $IMPOSM3_MAPPING
 
 # Create the imposm3 config file
 (
   echo "{"
-  echo "    \"cachedir\": \"$IMPSOSM3_CACHE_DIR\","
+  echo "    \"cachedir\": \"$IMPOSM3_CACHE_DIR\","
+  echo "    \"diffdir\": \"$IMPOSM3_DIFF_DIR\","
   echo "    \"connection\": \"postgis://$POSTGRES_USER:$POSTGRES_PASS@$POSTGRES_HOST/$POSTGRES_DB\","
   echo "    \"mapping\": \"$CONFIG_DIR/mapping.yml\""
   echo "}"
 
-) > $EXPORT_DIR/config.json
+) > $IMPOSM3_CONFIG
 
 #Download the planet pdb
-if [ ! -f data/planet-latest.osm.pbf ]; then
-    wget http://planet.osm.org/pbf/planet-latest.osm.pbf -P data
+if [ ! -f data/$PBF ]; then
+    wget $PBF_BASE$PBF -P data
 fi
 
-#Import the pdf to postgress using imposm3
-imposm3/bin/imposm import -config $EXPORT_DIR/config.json -read data/planet-latest.osm.pbf -write -optimize -overwritecache -diff
-imposm3/bin/imposm import -config $EXPORT_DIR/config.json -deployproduction
+diff_flag=""
+if [ "$DIFF_MODE" = true ]; then
+	diff_flag="-diff"
+	echo "Importing in diff mode"
+else
+	echo "Importing in normal mode"
+fi
 
-echo "====> : End importing Planet OpenStreetMap data: ./data/planet-latest.osm.pbf -> imposm3[./config/mapping.yaml] -> PostgreSQL"
+imposm3/bin/imposm import -config $IMPOSM3_CONFIG -overwritecache -read data/$PBF -deployproduction -write $diff_flag
+
+echo "====> : End importing Planet OpenStreetMap data: data/$PBF -> imposm3[./config/mapping.yaml] -> PostgreSQL"
