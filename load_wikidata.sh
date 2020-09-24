aed0ea5bc997d80b44000da748543dd79bc0a33f
Andrew Calcutt <acalcutt@techidiots.net>
2020-09-24 08:33:55 -0400

    Update load_wikidata.sh

--- load_wikidata.sh
+++ load_wikidata.sh
@@ -9,13 +9,4 @@ CACHE_DIR=$EXPORT_DIR/cache
 mkdir -p $EXPORT_DIR
 mkdir -p $CACHE_DIR
 
-
-
 import-wikidata --pghost="$POSTGRES_HOST" --pgport=$POSTGRES_PORT --dbname="$POSTGRES_DB" --user="$POSTGRES_USER" --password="$POSTGRES_PASS" --cache $CACHE_DIR/wikidata-cache.json $LAYER_FILE
-#[ ! -f data/latest-all.json.gz ] && wget https://dumps.wikimedia.org/wikidatawiki/entities/latest-all.json.gz -P data
-
-#if [ -f data/latest-all.json.gz ]; then
-#import-wikidata/import_wikidata --file="data/latest-all.json.gz" --host="$POSTGRES_HOST" --port=$POSTGRES_PORT --dbname="$POSTGRES_DB" --user="$POSTGRES_USER" --password="$POSTGRES_PASS"
-
-
-#fi
