ee6d65d93da1748c53f7b5565f9b39ba55202d7f
acalcutt <acalcutt@techidiots.net>
2021-01-25 19:27:48 -0500

    Update load_naturalearth.sh

--- load_naturalearth.sh
+++ load_naturalearth.sh
@@ -2,14 +2,21 @@
 . config/env.config
 echo "====> : Start importing  http://www.naturalearthdata.com  into PostgreSQL "
 
-#Import Natuaral Earth
-[ -f data/natural_earth_vector.sqlite ] && rm data/natural_earth_vector.sqlite 
-[ ! -f data/natural_earth_vector.sqlite.zip ] && wget http://naciscdn.org/naturalearth/packages/natural_earth_vector.sqlite.zip -P data
-[ -f data/natural_earth_vector.sqlite.zip ] && unzip -p data/natural_earth_vector.sqlite.zip packages/natural_earth_vector.sqlite > data/natural_earth_vector.sqlite 
+#--- From https://www.naturalearthdata.com/downloads/10m-physical-vectors/ ---
+if [ ! -f data/10m_physical.zip ]; then wget https://www.naturalearthdata.com/http//www.naturalearthdata.com/download/10m/physical/10m_physical.zi$
+unzip data/10m_physical.zip -d data/10m_physical
+for i in `find data/10m_physical -name "*.shp" -type f`; do
+        table=$(basename -- ${i%.*})
+        PGCONN="dbname=$POSTGRES_DB user=$POSTGRES_USER host=$POSTGRES_HOST password=$POSTGRES_PASS port=$POSTGRES_PORT"
+        PGCLIENTENCODING=UTF8 ogr2ogr -f Postgresql -s_srs EPSG:4326 -t_srs EPSG:3857 PG:"$PGCONN" -nlt PROMOTE_TO_MULTI -nln "$table" -skipfailur$
+done
+rm -rf 10m_physical
 
-if [ -f data/natural_earth_vector.sqlite ]; then
-	PGCONN="dbname=$POSTGRES_DB user=$POSTGRES_USER host=$POSTGRES_HOST password=$POSTGRES_PASS port=$POSTGRES_PORT"
-	PGCLIENTENCODING=LATIN1 ogr2ogr -progress -f Postgresql -s_srs EPSG:4326 -t_srs EPSG:3857 -clipsrc -180.1 -85.0511 180.1 85.0511 PG:"$PGCONN" -lco GEOMETRY_NAME=geometry -lco OVERWRITE=YES -lco DIM=2 -nlt GEOMETRY -overwrite data/natural_earth_vector.sqlite
-fi
-
-echo "====> : End importing  http://www.naturalearthdata.com  into PostgreSQL "
+#--- From https://www.naturalearthdata.com/downloads/10m-cultural-vectors/ ---
+if [ ! -f data/10m_cultural.zip ]; then wget https://www.naturalearthdata.com/http//www.naturalearthdata.com/download/10m/cultural/10m_cultural.zi$
+unzip data/10m_cultural.zip -d data/10m_cultural
+for i in `find data/10m_cultural -name "*.shp" -type f`; do
+        table=$(basename -- ${i%.*})
+        PGCONN="dbname=$POSTGRES_DB user=$POSTGRES_USER host=$POSTGRES_HOST password=$POSTGRES_PASS port=$POSTGRES_PORT"
+        PGCLIENTENCODING=UTF8 ogr2ogr -f Postgresql -s_srs EPSG:4326 -t_srs EPSG:3857 PG:"$PGCONN" -nlt PROMOTE_TO_MULTI -nln "$table" -skipfailur$
+done
