648873b461ec06ab045456a8f1f22a92455db7b3
acalcutt <acalcutt@techidiots.net>
2021-02-23 13:52:46 -0500

    try to make an updated osmborder_lines.csv

--- load_osmborder.sh
+++ load_osmborder.sh
@@ -1,6 +1,37 @@
 #!/bin/bash
 . config/env.config
+
+EXPORT_DIR=$(pwd)/data
+FULL_PBF=$EXPORT_DIR/planet-latest.osm.pbf
+OSMB_PBF=$EXPORT_DIR/osmborder_lines.pbf
+OSMB_CSV=$EXPORT_DIR/osmborder_lines.csv
+
+#Download the planet pdb if it does not exist
+if [ ! -f $FULL_PBF ]; then
+	echo "====> : Downloading PBF $FULL_PBF"
+	download-osm planet -o $FULL_PBF
+fi
+
+#Download pgfutter
+if [ ! -f $EXPORT_DIR/pgfutter ]; then
+	wget -O $EXPORT_DIR/pgfutter https://github.com/lukasmartinelli/pgfutter/releases/download/v1.1/pgfutter_linux_amd64
+	chmod +x $EXPORT_DIR/pgfutter
+fi
+
+#Filter the full PBF down using osmborder_filter
+echo "====> : Filtering border from PBF $FULL_PBF to $OSMB_PBF"
+if [ -f $OSMB_PBF ]; then rm -f $OSMB_PBF; fi
+osmborder_filter -o $OSMB_PBF $FULL_PBF
+
+#Create osmborder_lines.csv from filtered PBF
+echo "====> : Creating CSV $OSMB_CSV from $OSMB_PBF"
+if [ -f $OSMB_CSV ]; then rm -f $OSMB_CSV; fi
+osmborder -o $OSMB_CSV $OSMB_PBF
+rm -f $OSMB_PBF
+
 echo "====> : Start importing border data from http://openstreetmap.org into PostgreSQL "
+
+if [ -f $OSMB_CSV ]; then
 
 ob_table_name="osm_border_linestring"
 gen1_table_name="osm_border_linestring_gen1"
@@ -14,22 +45,14 @@ gen8_table_name="osm_border_linestring_gen8"
 gen9_table_name="osm_border_linestring_gen9"
 gen10_table_name="osm_border_linestring_gen10"
 
-#Download Files
-[ ! -f data/pgfutter ] && wget -O data/pgfutter https://github.com/lukasmartinelli/pgfutter/releases/download/v1.1/pgfutter_linux_amd64
-[ ! -f data/osmborder_lines.csv.gz ] && wget https://github.com/openmaptiles/import-osmborder/releases/download/v0.4/osmborder_lines.csv.gz -P data
-[ -f data/osmborder_lines.csv.gz ] && gzip -d < data/osmborder_lines.csv.gz > data/osmborder_lines.csv
-	
-if [ -f data/osmborder_lines.csv ]; then
-
-#Drop existing osm_ocean_polygon
+#Drop existing osm_border_linestring
 PGPASSWORD=$POSTGRES_PASS psql -h $POSTGRES_HOST -d $POSTGRES_DB -U "$POSTGRES_USER" <<EOSQL
 	DROP TABLE IF EXISTS $ob_table_name;
     CREATE TABLE $ob_table_name (osm_id bigint, admin_level int, dividing_line bool, disputed bool, maritime bool, geometry Geometry(LineString, 3857));
     CREATE INDEX ON $ob_table_name USING gist (geometry);
 EOSQL
 
-chmod +x data/pgfutter
-data/pgfutter --schema "public" --host "$POSTGRES_HOST" --port "$POSTGRES_PORT" --dbname "$POSTGRES_DB" --username "$POSTGRES_USER" --pass "$POSTGRES_PASS" --table "$ob_table_name" csv --fields "osm_id,admin_level,dividing_line,disputed,maritime,geometry" --delimiter $'\t' data/osmborder_lines.csv
+$EXPORT_DIR/pgfutter --schema "public" --host "$POSTGRES_HOST" --port "$POSTGRES_PORT" --dbname "$POSTGRES_DB" --username "$POSTGRES_USER" --pass "$POSTGRES_PASS" --table "$ob_table_name" csv --fields "osm_id,admin_level,dividing_line,disputed,maritime,geometry" --delimiter $'\t' $OSMB_CSV
 
 #Create Generalized tables
 PGPASSWORD=$POSTGRES_PASS psql -h $POSTGRES_HOST -d $POSTGRES_DB -U "$POSTGRES_USER" <<EOSQL
