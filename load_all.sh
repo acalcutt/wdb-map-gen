ed46dea08645628cf7010844399c5061f964606b
acalcutt <acalcutt@techidiots.net>
2021-03-09 19:34:41 -0500

    Update load_all.sh

--- load_all.sh
+++ load_all.sh
@@ -1,9 +1,9 @@
 ./setup_db.sh
 ./create_border_csv.sh
+./create_centerlines.sh
 ./load_osmborder.sh
 ./load_water.sh
 ./load_naturalearth.sh
-./load_lakelines.sh
 ./load_planet.sh
 ./load_wikidata.sh
 ./load_sql.sh
