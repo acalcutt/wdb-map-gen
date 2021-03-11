191c105b99cf22db3741449c43892a3508ec0036
acalcutt <acalcutt@techidiots.net>
2021-03-11 12:14:48 -0500

    cleanup centerlines creation

--- create_centerlines.sh
+++ create_centerlines.sh
@@ -8,12 +8,10 @@ CENTERLINES_GEOJSON=$EXPORT_DIR/lake_centerline.geojson
 MAPPING_YAML=$CONFIG_DIR/lake_centerlines.yaml
 IMPOSM3_CACHE_DIR=$EXPORT_DIR/lake_centerlines_cache
 
-mkdir -p $IMPOSM3_CACHE_DIR
-
-
+if [ ! -d "$EXPORT_DIR" ]; then mkdir -p $EXPORT_DIR; fi
+if [ ! -d "$IMPOSM3_CACHE_DIR" ]; then mkdir -p $IMPOSM3_CACHE_DIR; fi
 
 if [ ! -f $CENTERLINES_GEOJSON ]; then
-	echo "====> : Creating border data from planet PBF"
 	#Download the planet pdb if it does not exist
 	if [ ! -f $FULL_PBF ]; then
 		echo "====> : Downloading PBF $FULL_PBF"
@@ -21,14 +19,17 @@ if [ ! -f $CENTERLINES_GEOJSON ]; then
 	fi
 	
 	if [ -f $FULL_PBF ]; then
+		echo "====> : Importing plantet PBF into postgresql using imposm"
 		PG_CONNECT="postgis://$POSTGRES_USER:$POSTGRES_PASS@$POSTGRES_HOST/$POSTGRES_DB"
 		DB_SCHEMA="public"
 
 		tools/imposm3/bin/imposm import -connection "$PG_CONNECT" -mapping "$MAPPING_YAML" -overwritecache -cachedir "$IMPOSM3_CACHE_DIR" -read "$FULL_PBF" -dbschema-import="$DB_SCHEMA" -write
-		
+		echo "====> : Exporting lake shapes into a shapefile"
 		query="SELECT osm_id, ST_SimplifyPreserveTopology(geometry, 100) AS geometry FROM osm_lake_polygon WHERE area > 2 * 1000 * 1000 AND ST_GeometryType(geometry)='ST_Polygon' AND name <> '' ORDER BY area DESC"
 		pgsql2shp -f "$CENTERLINES_SHP" -h "$POSTGRES_HOST" -u "$POSTGRES_USER" -P "$POSTGRES_PASS" "$POSTGRES_DB" "$query"
-		
+		echo "====> : Creating a lake_centerline.geojson file from the exported shapefile"
 		label_centerlines --output_driver GeoJSON "$CENTERLINES_SHP" "$CENTERLINES_GEOJSON"
 	fi
+else
+	echo "====> : $CENTERLINES_GEOJSON Already Exists. Please delete it or move it and try again."
 fi
