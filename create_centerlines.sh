2238bf31847b6d0bcf8285558bb7c80fff8ad7cb
acalcutt <acalcutt@techidiots.net>
2021-03-11 11:36:24 -0500

    cleanup file names

--- create_centerlines.sh
+++ create_centerlines.sh
@@ -3,6 +3,7 @@
 CONFIG_DIR=$(pwd)/config
 EXPORT_DIR=$(pwd)/data
 FULL_PBF=$EXPORT_DIR/planet-latest.osm.pbf
+CENTERLINES_SHP=$EXPORT_DIR/osm_lake_polygon.shp
 CENTERLINES_GEOJSON=$EXPORT_DIR/lake_centerline.geojson
 MAPPING_YAML=$CONFIG_DIR/lake_centerlines.yaml
 IMPOSM3_CACHE_DIR=$EXPORT_DIR/lake_centerlines_cache
@@ -25,10 +26,9 @@ if [ ! -f $CENTERLINES_GEOJSON ]; then
 
 		tools/imposm3/bin/imposm import -connection "$PG_CONNECT" -mapping "$MAPPING_YAML" -overwritecache -cachedir "$IMPOSM3_CACHE_DIR" -read "$FULL_PBF" -dbschema-import="$DB_SCHEMA" -write
 		
-		lake_shapefile="data/osm_lake_polygon.shp"
 		query="SELECT osm_id, ST_SimplifyPreserveTopology(geometry, 100) AS geometry FROM osm_lake_polygon WHERE area > 2 * 1000 * 1000 AND ST_GeometryType(geometry)='ST_Polygon' AND name <> '' ORDER BY area DESC"
-		pgsql2shp -f "$lake_shapefile" -h "$POSTGRES_HOST" -u "$POSTGRES_USER" -P "$POSTGRES_PASS" "$POSTGRES_DB" "$query"
+		pgsql2shp -f "$CENTERLINES_SHP" -h "$POSTGRES_HOST" -u "$POSTGRES_USER" -P "$POSTGRES_PASS" "$POSTGRES_DB" "$query"
 		
-		label_centerlines --output_driver GeoJSON "data/osm_lake_polygon.shp" "data/osm_lake_centerline.geojson"
+		label_centerlines --output_driver GeoJSON "$CENTERLINES_SHP" "$CENTERLINES_GEOJSON"
 	fi
 fi
