e9626d0ee09e2d4c4618deb97cdde18a3828e886
acalcutt <acalcutt@techidiots.net>
2021-03-11 11:35:53 -0500

    remove '-s_srs' from ogr2og cmd so my geojson file imports

--- load_water.sh
+++ load_water.sh
@@ -4,6 +4,8 @@
 PGCONN="dbname=$POSTGRES_DB user=$POSTGRES_USER host=$POSTGRES_HOST password=$POSTGRES_PASS port=$POSTGRES_PORT"
 WATER_TABLE_NAME="osm_ocean_polygon"
 LAKE_CENTERLINE_TABLE="lake_centerline"
+EXPORT_DIR=$(pwd)/data
+CENTERLINES_GEOJSON=$EXPORT_DIR/lake_centerline.geojson
 
 echo "====> : Start importing water data from https://osmdata.openstreetmap.de/data/water-polygons.html into PostgreSQL "
 
@@ -23,18 +25,12 @@ DB_SCHEMA="public"
 
 tools/imposm3/bin/imposm import -connection "$PG_CONNECT" -mapping "$MAPPING_YAML" -overwritecache -cachedir "$IMPOSM_CACHE_DIR" -read "$pbf_file" -dbschema-import="$DB_SCHEMA" -write
 
-#lake_shapefile="/data/osm_lake_polygon.shp"
-#query="SELECT osm_id, ST_SimplifyPreserveTopology(geometry, 100) AS geometry FROM osm_lake_polygon WHERE area > 2 * 1000 * 1000 AND ST_GeometryType(geometry)='ST_Polygon' AND name <> '' ORDER BY area DESC"
-#pgsql2shp -f "$lake_shapefile" -h "$POSTGRES_PORT_5432_TCP_ADDR" -u "$POSTGRES_ENV_POSTGRES_USER" -P "$POSTGRES_ENV_POSTGRES_PASSWORD" "$POSTGRES_DB" "$query"
+#Load Lake CenterLines
 
-#create_centerlines --output_driver "GeoJSON" "/data/osm_lake_polygon.shp" "/data/osm_lake_centerline.geojson"
+[ ! -f $CENTERLINES_GEOJSON ] && wget https://github.com/acalcutt/osm-lakelines/releases/download/v9.1/lake_centerline.geojson -P data
 
-
-
-[ ! -f data/lake_centerline.geojson ] && wget https://github.com/acalcutt/osm-lakelines/releases/download/v9.1/osm_lake_centerline.geojson -P data
-
-if [ -f data/lake_centerline.geojson ]; then
-	PGCLIENTENCODING=UTF8 ogr2ogr -progress -f Postgresql -s_srs EPSG:4326 -t_srs EPSG:3857 "PG:$PGCONN" -lco OVERWRITE=YES -overwrite -nln "$LAKE_CENTERLINE_TABLE" data/lake_centerline.geojson
+if [ -f $CENTERLINES_GEOJSON ]; then
+	PGCLIENTENCODING=UTF8 ogr2ogr -progress -f Postgresql -t_srs EPSG:3857 "PG:$PGCONN" -lco OVERWRITE=YES -overwrite -nln "$LAKE_CENTERLINE_TABLE" "data/lake_centerline.geojson"
 fi
 
 echo "====> : End importing OpenStreetMap Lakelines data "
