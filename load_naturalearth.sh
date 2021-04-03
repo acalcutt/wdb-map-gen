587b18c2f7e5546b9760e751216e18d296fa8ddd
acalcutt <acalcutt@techidiots.net>
2021-04-02 22:06:19 -0400

    revert some code

--- load_naturalearth.sh
+++ load_naturalearth.sh
@@ -5,6 +5,7 @@
 PGCONN="dbname=$POSTGRES_DB user=$POSTGRES_USER host=$POSTGRES_HOST password=$POSTGRES_PASS port=$POSTGRES_PORT"
 EXPORT_DIR=$(pwd)/data
 NE_EXTRACT=$EXPORT_DIR/ne_extract
+NATURAL_EARTH_DB=$EXPORT_DIR/natural_earth_vector.sqlite
 
 #Set up direcotries
 if [ -d "$NE_EXTRACT" ]; then rm -Rf $NE_EXTRACT; fi
@@ -29,13 +30,18 @@ unzip -o $EXPORT_DIR/50m_cultural.zip -d $NE_EXTRACT/50m_cultural
 unzip -o $EXPORT_DIR/110m_physical.zip -d $NE_EXTRACT/110m_physical
 unzip -o $EXPORT_DIR/110m_cultural.zip -d $NE_EXTRACT/110m_cultural
 
-#Import extracted share files into Postgresql
+#Import extracted share files into sqlite with ogr2ogr
 echo "====> : Importing shape files into Postgresql"
 for i in `find $NE_EXTRACT -name "*.shp" -type f`; do
-        table=$(basename -- ${i%.*})
-		echo "Importing '$i' into '$table'"
-        PGCLIENTENCODING=UTF8 ogr2ogr -progress -f Postgresql -s_srs EPSG:3857 -t_srs EPSG:3857 -lco OVERWRITE=YES -lco GEOMETRY_NAME=geometry -overwrite -nln "$table" -nlt geometry --config PG_USE_COPY YES PG:"$PGCONN" $i
+        #table=$(basename -- ${i%.*})
+		#echo "Importing '$i' into '$table'"
+        #PGCLIENTENCODING=UTF8 ogr2ogr -progress -f Postgresql -s_srs EPSG:3857 -t_srs EPSG:3857 -lco OVERWRITE=YES -lco GEOMETRY_NAME=geometry -overwrite -nln "$table" -nlt geometry --config PG_USE_COPY YES PG:"$PGCONN" $i
+		ogr2ogr -progress -f SQLite -append $NATURAL_EARTH_DB $i;
 done
+
+echo "====> : Start importing $NATURAL_EARTH_DB into Postgresql "
+echo $NATURAL_EARTH_DB;
+PGCLIENTENCODING=UTF8 ogr2ogr -progress -f Postgresql -s_srs EPSG:4326 -t_srs EPSG:3857 -clipsrc -180.1 -85.0511 180.1 85.0511 PG:"${PGCONN}" -lco GEOMETRY_NAME=geometry -lco OVERWRITE=YES -lco DIM=2 -nlt GEOMETRY -overwrite "$NATURAL_EARTH_DB"
 
 #Delete the extracted files
 echo "====> : Deleting extracted files"
