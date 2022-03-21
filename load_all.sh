e87efe37258c11e61a74ebf28fbd2e28adcc8932
acalcutt <acalcutt@techidiots.net>
2022-03-20 23:55:06 -0400

    remove old files

--- load_all.sh
+++
@@ -1,9 +0,0 @@
-./setup_db.sh
-./create_border_csv.sh
-./create_centerlines.sh
-./load_osmborder.sh
-./load_water.sh
-./load_naturalearth.sh
-./load_planet.sh
-./load_wikidata.sh
-./load_sql.sh
--- test-perf.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-. config/env.config
-
-CONFIG_DIR=$(pwd)/config
-LAYER_FILE=$CONFIG_DIR/$EXPORT_LAYER_FILE
-
-UV_THREADPOOL_SIZE=$UV_THREADPOOL_SIZE test-perf $LAYER_FILE --pghost="$POSTGRES_HOST" --pgport=$POSTGRES_PORT --dbname="$POSTGRES_DB" --user="$POSTGRES_USER" --password="$POSTGRES_PASS" --verbose
