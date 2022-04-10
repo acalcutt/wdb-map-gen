3649cc823e7d5eb0d175dd5577dc676f5042b67d
acalcutt <acalcutt@techidiots.net>
2022-04-10 10:43:34 -0400

    fix path

--- export_mbtiles.sh
+++ export_mbtiles.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 EXPORT_DIR=$(pwd)/data
+TMP_DIR=$(pwd)/data/tmp
+if [ -d $TMP_DIR ]; then rm -Rf $TMP_DIR; fi
+if [ -f $EXPORT_DIR/output.mbtiles ]; then rm -Rf $EXPORT_DIR/output.mbtiles; fi
 
 java -Xmx45g \
 	-XX:OnOutOfMemoryError="kill -9 %p" \
-	-jar /planetiler/planetiler-dist/target/planetiler-dist-0.4-SNAPSHOT-with-deps.jar \
+	-jar tools/planetiler/planetiler-dist/target/planetiler-dist-0.4-SNAPSHOT-with-deps.jar \
 	--area=planet --extra_layers=atv \
 	--mbtiles=$EXPORT_DIR/output.mbtiles \
 	--nodemap-type=array --nodemap-storage=mmap --nodemap-madvise --optimize_db=true
