5c00a10b991b219db96dae0513acb0a964466599
acalcutt <acalcutt@techidiots.net>
2022-04-10 02:13:07 -0400

    updated version, add area

--- export_mbtiles.sh
+++ export_mbtiles.sh
@@ -4,8 +4,8 @@ EXPORT_DIR=$(pwd)/data
 
 java -Xmx45g \
 	-XX:OnOutOfMemoryError="kill -9 %p" \
-	-jar /planetiler/planetiler-dist/target/planetiler-dist-0.3-SNAPSHOT-with-deps.jar \
-	--extra_layers=atv \
+	-jar /planetiler/planetiler-dist/target/planetiler-dist-0.4-SNAPSHOT-with-deps.jar \
+	--area=planet --extra_layers=atv \
 	--mbtiles=$EXPORT_DIR/output.mbtiles \
 	--nodemap-type=array --nodemap-storage=mmap --nodemap-madvise --optimize_db=true
 #	--nodemap-type=sparsearray --nodemap-storage=mmap --optimize_db=true
